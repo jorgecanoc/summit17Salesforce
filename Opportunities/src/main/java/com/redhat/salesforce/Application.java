cc73ec57f4d7736203038db858d4a79e01dd1499
weimeilin79 <weimeilin@gmail.com>
2017-04-17 11:28:25 -0400

    First commit

---
+++ Opportunities/src/main/java/com/redhat/salesforce/Application.java
@@ -0,0 +1,32 @@
+package com.redhat.salesforce;
+
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring/camel-context.xml"})
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+        
+    }
+
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(
+            new CamelHttpTransportServlet(), "/demo/*");
+        servlet.setName("CamelServlet");
+        return servlet;
+    }
+
+    
+
+   
+}
